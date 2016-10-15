6d013fb37c4af256586a405e1b0b4fa3f6b26934
Jérémy Berthelier <jeremyberthelier@hotmail.fr>
2016-10-15 09:41:56 +0000

    Update producer save

--- Blog/Actions/Producers/SaveAction.php
+++ Blog/Actions/Producers/SaveAction.php
@@ -19,19 +19,14 @@ class SaveAction extends Action
  public function onPost($request, $args)
  {
     $this->producer = R::dispense('producer');
-    $this->producer->name = $request->post->get('name');
-    $this->producer->slogan = $request->post->get('slogan');
-    $this->producer->description = $request->post->get('description');
-    $this->producer->category = $request->post->get('category');
-    $this->producer->phone = $request->post->get('phone');
-    $this->producer->email = $request->post->get('email');
-    $this->producer->website = $request->post->get('website');
-    $this->producer->adress = $request->post->get('adress');
-    $this->producer->zipcode = $request->post->get('zipcode');
-    $this->producer->city = $request->post->get('city');
-    $this->producer->posLat = $request->post->get('posLat');
-    $this->producer->posLong = $request->post->get('posLong');
-
+    
+    $post = (array) $request->post;
+    
+    foreach (array_shift($post) as $key=>$value)
+    {
+     $this->producer->$key = $value;
+    }
+    
     $id = R::store($this->producer);
     
    $this->redirect('/');
