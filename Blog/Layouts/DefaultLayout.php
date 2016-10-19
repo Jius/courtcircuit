f480afba49703f567a4d1f4a8c6589c13161d17c
Jérémy Berthelier <jeremyberthelier@hotmail.fr>
2016-10-19 16:09:48 +0000

    Add Materialize style, Evolve producer form

--- Blog/Layouts/DefaultLayout.php
+++ Blog/Layouts/DefaultLayout.php
@@ -11,11 +11,75 @@ class DefaultLayout extends Layout
   <head>
     <meta charset="utf-8">
     <title>Court-circuit</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+    
   </head>
   <body>
-    <div>
+    <div class="container">
       {{{content}}}
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+    <script>
+      $(document).ready(function() {
+        $('select').material_select();
+        
+        //Init step form producer
+        $('.step-prev').fadeOut();
+        
+        var cpt=1, steps = $('.step').length;
+        $('.step').each(function() {
+          $(this).attr('data-step', cpt);
+          if (cpt>1) {
+            $(this).fadeOut();
+          }
+          cpt++;
+        });
+        if (steps>1) {
+          $('.step-next').attr('link-step', 2);
+        }
+        
+        //END INIT
+         
+         $('.step-next').click(function() {
+          var target=parseInt($(this).attr('link-step'), 10);
+          if (target - 1 > 0) {
+            $('.step-prev').fadeIn();
+          }
+          
+          $('.step-prev').attr('link-step', target-1);
+          $('.step[data-step='+(target - 1)+']').hide();
+          $('.step[data-step='+(target)+']').fadeIn();
+          
+          
+          if (target + 1 <= steps) {
+            $(this).attr('link-step', target+1);
+          } else {
+            $(this).hide();
+          }
+         });
+         
+         $('.step-prev').click(function() {
+          var target=parseInt($(this).attr('link-step'), 10);
+          
+          if (target - 1 <= 0) {
+            $(this).hide();
+          } else {
+            $(this).attr('link-step', target-1);
+          }
+          
+          $('.step-next').attr('link-step', target + 1);
+          $('.step[data-step='+(target + 1)+']').hide();
+          $('.step[data-step='+(target)+']').fadeIn();
+          
+          if (target + 1 <= steps && $('.step-next').not(":visible")) {
+            $('.step-next').fadeIn();
+          }
+          
+         });
+      });
+    </script>
   </body>
 </html>
 HTML;
--- Blog/Views/ProducerNewView.php
+++ Blog/Views/ProducerNewView.php
@@ -12,85 +12,134 @@ class ProducerNewView extends View
   ];
   
   const TPL = <<<HTML
-<form action="/pro/create" method="post">
-  <label for="name">Nom de l entreprise: </label>
-  <input placeholder="Placeholder" id="name" type="text" name="name">
-  
-  <br/>
-  <label for="slogan">Slogan: </label>
-  <input placeholder="Placeholder" id="slogan" type="text" name="slogan">
-  
-  <br/>
-  <label for="description">Quelques mots sur l entreprise: </label>
-  <textarea  id="description" name="description"></textarea>
-  
-  <br/>
-  <label for="category">Catégorie: </label>
-  <select name="category">
-    <option value="" disabled selected>Choississez une catégorie</option>
-    {{#categories}}
-      <option value="{{id}}">{{title}}</option> 
-    {{/categories}}
-    {{^categories}}
-      <p>Aucune catégorie n'a été ajoutée</p>
-    {{/categories}}
-  </select>
-  
-  <br/>
-  <label for="labels">Label(s): </label>
-  <select name="labels">
-    <option value="" disabled selected>Choississez un ou des labels</option>
-    {{#labels}}
-      <option value="{{id}}">{{title}}</option> 
-    {{/labels}}
-    {{^labels}}
-      <p>Aucun label n'a été ajoutée</p>
-    {{/labels}}
-  </select>
-  <br/>
-  
-  <label for="tags">Tags (séparé par un ;) : </label>
-  <input placeholder="Placeholder" id="tags" type="text" name="tags">
-  
-  <br/>
-  <label for="phone">Téléphone: </label>
-  <input placeholder="Placeholder" id="phone" type="text" name="phone">
-  
-  <br/>
-  <label for="email">Email: </label>
-  <input placeholder="Placeholder" id="email" type="text" name="email">
-  
-  <br/>
-  <label for="website">Site web: </label>
-  <input placeholder="Placeholder" id="website" type="text" name="website">
-  
-  <br/>
-  <label for="owner">Nom(s) propriétaire(s): </label>
-  <textarea  id="owner" name="owner"></textarea>
-  
-  <br/>
-  <label for="adress">Adresse: </label>
-  <input placeholder="Placeholder" id="adress" type="text" name="adress">
-  
-  <br/>
-  <label for="zipcode">Code postal: </label>
-  <input placeholder="Placeholder" id="zipcode" type="text" name="zipcode">
-  
-  <br/>
-  <label for="city">Ville: </label>
-  <input placeholder="Placeholder" id="city" type="text" name="city">
-  
-  <br/>
-  <label for="posLat">Position Latitude: </label>
-  <input placeholder="Placeholder" id="posLat" type="text" name="posLat">
-  
-  <br/>
-  <label for="posLong">Position Longitudinal: </label>
-  <input placeholder="Placeholder" id="posLong" type="text" name="posLong">
-  
-  <br/>
-  <button type="submit" name="action">Submit</button
-</form>
+<div class="row">
+  <form action="/pro/create" method="post" class="col s6 offset-s3">
+    <div class="step">
+      <div class="row">
+        <div class="input-field col s6">
+          <input placeholder="Placeholder" id="name" type="text" name="name" class="validate">
+          <label for="name">Nom de l entreprise: </label>
+        </div>
+        
+        <div class="input-field col s6">
+          <select name="category">
+            <option value="" disabled selected>Choississez une catégorie</option>
+            {{#categories}}
+              <option value="{{id}}">{{title}}</option> 
+            {{/categories}}
+            {{^categories}}
+              <p>Aucune catégorie n'a été ajoutée</p>
+            {{/categories}}
+          </select>
+          <label for="category">Catégorie: </label>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="input-field col s12">
+          <textarea  id="description" name="description" class="materialize-textarea"></textarea>
+          <label for="description">Que proposez vous en produits locaux ? </label>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="input-field col s12">
+          <select multiple name="labels">
+            <option value="" disabled selected>Possèdez vous des labels ?</option>
+            {{#labels}}
+              <option value="{{id}}">{{title}}</option> 
+            {{/labels}}
+            {{^labels}}
+              <p>Aucun label n'a été ajoutée</p>
+            {{/labels}}
+          </select>
+          <label for="labels">Label(s): </label>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="input-field col s12">
+          <input placeholder="Placeholder" id="tags" type="text" name="tags" class="validate">
+          <label for="tags">Remplissez quelques mots-clés (séparé par un ;) afin de mieux vous retrouver. </label>
+        </div>
+      </div>
+    </div>
+    
+    <div class="step">
+      <div class="row">
+        <div class="input-field col s12">
+          <input placeholder="Placeholder" id="slogan" type="text" name="slogan" class="validate">
+          <label for="slogan">Avez vous un slogan ou un phrase d'accroche ? (qui sera présent sur votre page de présentation de votre boutique)</label>
+        </div>
+      </div>
+    
+      <div class="row">
+        <div class="input-field col s12">
+          <textarea  id="owner" name="owner" class="materialize-textarea"></textarea>
+          <label for="owner">Nom(s) du ou des propriétaire(s): </label>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="input-field col s6">
+          <input placeholder="Placeholder" id="adress" type="text" name="adress" class="validate">
+          <label for="adress">Adresse (de la boutique): </label>
+        </div>
+        
+        <div class="input-field col s3">
+          <input placeholder="Placeholder" id="zipcode" type="text" name="zipcode" class="validate">
+          <label for="zipcode">Code postal: </label>
+        </div>
+        
+        <div class="input-field col s3">
+          <input placeholder="Placeholder" id="city" type="text" name="city" class="validate">
+          <label for="city">Ville: </label>
+        </div>
+      </div>
+    </div>
+    
+    <div class="step">
+      <div class="row">
+        <div class="input-field col s5">
+          <input placeholder="Placeholder" id="email" type="text" name="email" class="validate">
+          <label for="email">Email: </label>
+        </div>
+        
+        <div class="input-field col s3">
+          <input placeholder="Placeholder" id="phone" type="text" name="phone" class="validate">
+          <label for="phone">Téléphone: </label>
+        </div>
+        
+        
+        <div class="input-field col s4">
+          <input placeholder="Placeholder" id="website" type="text" name="website" class="validate">
+          <label for="website">Site web: </label>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="input-field col s6">
+          <input placeholder="Placeholder" id="posLat" type="text" name="posLat" class="validate">
+          <label for="posLat">Position Latitude: </label>
+        </div>
+        <div class="input-field col s6">
+          <input placeholder="Placeholder" id="posLong" type="text" name="posLong" class="validate">
+          <label for="posLong">Position Longitudinal: </label>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col s4 offset-s8">
+          <button class="btn waves-effect waves-light" type="submit" name="action">Enregistrer
+            <i class="material-icons right">send</i>
+          </button>
+        </div>
+      </div>
+    </div>
+    <a class="waves-effect waves-light btn step-prev step-btn blue lighten-1"><i class="material-icons left">keyboard_arrow_left</i>Retour</a>
+    <a class="waves-effect waves-light btn step-next step-btn blue lighten-1"><i class="material-icons right">keyboard_arrow_right</i>Suivant</a>
+  </form>
+</div>
 HTML;
 
 
