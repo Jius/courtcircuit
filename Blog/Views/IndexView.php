fa9c4ccf91f4baf96fa49b209f316061d88afd85
jius <jeremyberthelier@hotmail.fr>
2016-10-24 10:13:54 +0000

    Adapt index for mobile

--- Blog/Views/IndexView.php
+++ Blog/Views/IndexView.php
@@ -13,15 +13,14 @@ class IndexView extends View
   <div class="row">
     <form class="col s12">
       <div class="row m-t-med">
-        <div class="col s4 hide-on-med-and-down"></div>
-        <div class="input-field col s12 m12 l4">
+        <div class="input-field col s12">
           <input id="q" name="q" class="search white" placeholder="Que recherchez-vous ?" type="text">
-        </div>
-        <div class="input-field search-btn col s12 m12 l4">
-          <button class="btn-floating btn-large waves-effect waves-light orange darken-4" type="submit" name="action">
-            <i class="material-icons">search</i>
-          </button>
-          <a class="btn-floating btn-large waves-effect waves-light white my-location"><i class="material-icons">my_location</i></a>
+          <div class="search-btn">
+            <button class="btn-floating btn-large waves-effect waves-light orange darken-4" type="submit" name="action">
+              <i class="material-icons">search</i>
+            </button>
+            <a class="btn-floating btn-large waves-effect waves-light white my-location"><i class="material-icons">my_location</i></a>
+          </div>
         </div>
       </div>
     </form>
