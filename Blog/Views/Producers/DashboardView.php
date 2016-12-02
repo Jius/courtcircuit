0cd9c11244b26cd667fab6bfaa4e6551764df41d
jius <jeremyberthelier@hotmail.fr>
2016-12-02 14:44:01 +0000

    Adding shops by producer on his dashboard - CSS

--- Blog/Views/Producers/DashboardView.php
+++ Blog/Views/Producers/DashboardView.php
@@ -12,60 +12,68 @@ class DashboardView extends View
   ];
   
   const TPL = <<<HTML
-<div class="center-align head-dash">
-  <h2>Tableau de Bord</h2>
-  {{#user}}<p class="title medium">Bonjour {{firstname}}</p>{{/user}}
-</div>
-
-<div class="row shop-container">
-  <div class="col s12">
-    <p class="title big blue-text dash-lab">Vos boutiques</p>
-    <a class="btn-floating btn-large waves-effect waves-light red" href="/pro/boutique"><i class="material-icons">add</i></a>
-    
-    <div class="row">
-    
-      {{#shops}}
-      {{/shops}}
+  <div class="center-align head-dash">
+    <h2>Tableau de Bord</h2>
+    {{#user}}<p class="title medium">Bonjour {{firstname}}</p>{{/user}}
+  </div>
+  
+  <div class="row shop-container">
+    <div class="col s12">
+      <p class="title big blue-text dash-lab">Vos boutiques</p>
+      <a class="btn-floating btn-large waves-effect waves-light red" href="/pro/boutique"><i class="material-icons">add</i></a>
       
-      {{^shops}}
-        <div class="col s12">
-          <p>Aucunes boutiques enregistrées.</p>
-        </div>
-      {{/shops}}
+      <div class="row shops-card">
+      
+        {{#shops?}}
+          <div class="center-align col s3">
+            <div class="shop white z-depth-3">
+              <a href="/shop/{{id}}">{{title}}</a>
+            </div>
+          </div>
+        {{/shops?}}
+        
+        {{^shops?}}
+          <div class="col s12">
+            <p>Aucunes boutiques enregistrées.</p>
+          </div>
+        {{/shops?}}
+        
+      </div>
       
     </div>
     
   </div>
   
-</div>
-
-<div class="row event-container">
-  <div class="col s12">
-    <p class="title big blue-text dash-lab">Evénements & Déplacements</p>
-    <a class="btn-floating btn-large waves-effect waves-light purple"><i class="material-icons">add</i></a>
-    
-    <div class="row">
-    
-      {{#events}}
-      {{/events}}
+  <div class="row event-container">
+    <div class="col s12">
+      <p class="title big blue-text dash-lab">Evénements & Déplacements</p>
+      <a class="btn-floating btn-large waves-effect waves-light purple"><i class="material-icons">add</i></a>
       
-      {{^events}}
-        <div class="col s12">
-          <p>Aucuns déplacements enregistrés.</p>
-        </div>
-      {{/events}}
+      <div class="row">
+      
+        {{#events}}
+        {{/events}}
+        
+        {{^events}}
+          <div class="col s12">
+            <p>Aucuns déplacements enregistrés.</p>
+          </div>
+        {{/events}}
+        
+      </div>
       
     </div>
-    
   </div>
-</div>
 HTML;
 
 
   public function render()
   {
+    $user = Session::load()->get('user');
+    $shops = R::find('shops','owner = ?', [ $user["id"] ]);
     return [
-      'user' => Session::load()->get('user')
+      'user' => $user,
+      'shops?' => array_values($shops)
     ];
   }
 }
