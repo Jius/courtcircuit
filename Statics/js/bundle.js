fade1a05bdaaee0779e560aa9321fa910259261a
jius <jeremyberthelier@hotmail.fr>
2016-12-16 13:35:13 +0000

    Add timepicker for openning shop + translate in French the datepicker

--- Statics/js/bundle.js
+++ Statics/js/bundle.js
@@ -8,13 +8,22 @@ $(document).ready(function() {
   });
   
   $('.datepicker').pickadate({
-    monthsFull: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
-    weekdaysFull: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
-    weekdaysShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year
+    monthsFull: [ 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre' ],
+    monthsShort: [ 'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec' ],
+    weekdaysFull: [ 'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi' ],
+    weekdaysShort: [ 'Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam' ],
+    today: 'Aujourd\'hui',
+    clear: 'Effacer',
+    close: 'Fermer',
+    firstDay: 1,
     format: 'Le dddd dd mmmm yyyy',
     formatSubmit: 'yyyy-mm-dd',
+    labelMonthNext:"Mois suivant",
+    labelMonthPrev:"Mois précédent",
+    labelMonthSelect:"Sélectionner un mois",
+    labelYearSelect:"Sélectionner une année",
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year
     onSet: function( arg ){
       if ( 'select' in arg ){ //prevent closing on selecting month/year
           this.close();
